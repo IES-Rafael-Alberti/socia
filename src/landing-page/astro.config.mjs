5c880d41d53db09e3e8cd006b625b52640a66a77
David <17435126+DavidLMS@users.noreply.github.com>
2025-06-18 09:09:51 +0200

    update urls

--- src/landing-page/astro.config.mjs
+++ src/landing-page/astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://socia-platform.netlify.app/",
+  site: "https://ies-rafael-alberti.github.io",
+  base: "/socia",
   integrations: [tailwind(), icon()],
   i18n: {
     defaultLocale: "es",
